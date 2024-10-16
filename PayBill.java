6efc52291ef4f67923c266825d449589214076fe
mahesh29-sudo <155453330+mahesh29-sudo@users.noreply.github.com>
2024-10-16 12:39:38 +0530

    Add files via upload

---
+++ PayBill.java
@@ -0,0 +1,30 @@
+import java.awt.Dimension;
+
+import javax.swing.JEditorPane;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+
+public class PayBill extends JFrame {
+
+	PayBill(){
+	    JEditorPane j = new JEditorPane();
+        j.setEditable(false);
+
+        try {
+            j.setPage("https://paytm.com/electricity-bill-payment");
+        }catch (Exception e) {
+            j.setContentType("text/html");
+            j.setText("<html>Could not load</html>");
+        }
+        JScrollPane scrollPane = new JScrollPane(j);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getContentPane().add(scrollPane);
+        setPreferredSize(new Dimension(800,600));
+        setSize(800,800);
+        setLocation(250,120);
+        setVisible(true);
+	}
+	 public static void main(String[] args){
+	        new PayBill().setVisible(true);
+	    }
+}
